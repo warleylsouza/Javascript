283765c7b387f9971cd8da79de5824730d1c4a0d
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-04 12:05:59 -0300

    ex009 exx010

---
+++ aula010/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 68
+console.log(`A velocidade do seu carro é  ${vel}Km/h`)
+if (vel > 60){//condição simples!
+    console.log(`Voce ultrapassou o limite de velocidade.MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
