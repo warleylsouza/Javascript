dccb456f6d2efa5dbdcbd6740160ce07b60c025d
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-07 13:59:08 -0300

    atualização

--- ex014/aula 12 ex/script.js
+++ ex014/aula 12 ex/script.js
@@ -1,1 +1,25 @@
-alert('ola!')
+
+function carregar(){
+
+var msg = window.document.getElementById('msg')
+var img = window.document.getElementById('imagem')
+var data = new Date()
+//var hora = data.getHours()
+var hora = 11 //forçando
+msg.innerHTML = `Agora são ${hora} horas.`
+
+if (hora >= 0 && hora < 12 ) {
+//bom dia!
+    img.src = 'imagens/dia.png'
+document.body.style.background = '#e2cd9f'
+} else if (hora >= 12 && hora <= 18) {
+    //Boa Tarde!
+    img.src = 'imagens/tarde.png'
+    document.body.style.background = '#b9846f'
+} else  {
+    //Boa Noite!
+    img.src = 'imagens/noite.png'
+    document.body.style.background = '#515154'
+
+}
+}
