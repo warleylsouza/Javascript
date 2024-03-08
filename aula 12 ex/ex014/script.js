994c1b2c3149bbae799b5340fee0a11edfea1bed
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-08 15:06:18 -0300

    Update script.js

--- aula 12 ex/ex014/script.js
+++ aula 12 ex/ex014/script.js
@@ -5,7 +5,7 @@ var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
 //var hora = data.getHours()
-var hora = 11//forçando
+var hora = 15//forçando
 msg.innerHTML = `Agora são ${hora} horas.`
 
 if (hora >= 0 && hora < 12 ) {
