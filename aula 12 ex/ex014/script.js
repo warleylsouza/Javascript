e59ae28f052320a1f1a4257e2f8f153fb9235609
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-07 21:20:55 -0300

    atualização

--- aula 12 ex/ex014/script.js
+++ aula 12 ex/ex014/script.js
@@ -4,12 +4,12 @@ function carregar(){
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-//var hora = data.getHours()
-var hora = 23//forçando
+var hora = data.getHours()
+//var hora = 23//forçando
 msg.innerHTML = `Agora são ${hora} horas.`
 
 if (hora >= 0 && hora < 12 ) {
-//bom dia!
+//Bom dia!
     img.src = 'imagens/dia.png'
 document.body.style.background = '#e2cd9f'
 } else if (hora >= 12 && hora <= 18) {
