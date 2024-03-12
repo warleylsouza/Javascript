102ec56c809ab31246d18b8e6fcb09ac0677334f
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-12 16:54:52 -0300

    Update index.js

--- aula 13/index.js
+++ aula 13/index.js
@@ -1,20 +1,21 @@
 //Estrutura de repetição com teste logico no inicio
 
-/*var c = 1
-while (c <= 5){
-    console.log(`Passo ${c}`)
-    c++ // c = c + 1
+var c = 1
+while (c <= 5){ // Enquanto o valor de c = 2 for menor ou igual 5 , execute o loop ate for verdadeiro!
+    console.log(`Passo ${c}`) //place holder $
+    c++ // c = c + 1 c= 1 + 1 c=2
+                     
 }
 
-*/
+
 
 //Estrutura e repetição com teste logico no final
 
-var c = 1
+/*var c = 1
 do {
     console.log(`Passo ${c}`)
     c++ // c = c + 1
-} while (c <= 4)
+} while (c <= 4)*/
 
 
 //Não usar
