3fa9620882049d94fbafc0252d42a49daef757fd
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-12 15:50:05 -0300

    Update index.js

--- aula 13/index.js
+++ aula 13/index.js
@@ -1,10 +1,22 @@
-var c = 1
+//Estrutura de repetição com teste logico no inicio
+
+/*var c = 1
 while (c <= 5){
     console.log(`Passo ${c}`)
     c++ // c = c + 1
 }
 
+*/
 
+//Estrutura e repetição com teste logico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 4)
+
+
+//Não usar
 /*console.log('Tudo bem!')
 console.log('Tudo bem!')
 console.log('Tudo bem!')
