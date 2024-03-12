82bc000931bf6a8a8ae16390001c64b06f5951d8
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-12 15:37:08 -0300

    repeticoes js

---
+++ aula 13/index.js
@@ -0,0 +1,16 @@
+var c = 1
+while (c <= 5){
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+
+
+/*console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
+
