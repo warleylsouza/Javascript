44bec922aa82b18a2a274f722f992a071a98b5c8
warleylsouza <143962680+warleylsouza@users.noreply.github.com>
2024-03-24 17:04:36 -0300

    funcoes

--- aula 15/ambiente.js
+++ aula 15/ambiente.js
@@ -6,7 +6,7 @@ num.sort(); //colocar em ordem crescente os valores do vetor
 
 
 console.log(num);
-console.log(` O vetor tem  ${num.length} posições`);
+console.log(`O vetor tem  ${num.length} posições`);
 console.log(`O primeiro valor do vetor é ${num[0]}`);
 
 let pos = num.indexOf(5)
